5a6c4e387ca3740d006758473dbd8b93e57c547f
SaraAGithub-Portfolio <skausa29@gmail.com>
2023-10-23 13:59:39 +0100

    added a Transaction List component to list transactions for the user to see

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,9 +1,12 @@
 import { useAuthContext } from '../../hooks/useAuthContext';
 import styles from './Home.module.css';
 import TransactionForm from './TransactionForm';
+import { useCollection } from '../../hooks/useCollection';
+import TransactionList from './TransactionList';
 
 export default function Home() {
     const { user } = useAuthContext()
+    const { documents, error } = useCollection('transactions')
 
     // Extract the UID from the user object
     const uid = user ? user.uid : null;
@@ -11,7 +14,8 @@ export default function Home() {
     return (
         <div className={styles.container}>
             <div className={styles.content}>
-                Transaction List
+                {error && <p>{error}</p>}
+                {documents && <TransactionList transactions={documents} />}
             </div>
             <div className={styles.sidebar}>
                 <TransactionForm uid={uid} />
